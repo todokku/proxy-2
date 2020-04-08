c6990ad82d5dbcdf7650eb9a9ab8d2d7b2643425
wulala <l>
2020-04-08 15:58:04 +0800

    修复remove

--- proxy/rule/wx.js
+++ proxy/rule/wx.js
@@ -201,14 +201,14 @@ module.exports = {
                 let bodycontent = newResponse.body.toString("utf-8")
 
                 $ = cheerio.load(bodycontent)
-
-                $('#img-content').remove() // 删除正文
                 $('style').remove() // 删除样式
-                $('.rich_media_area_extra').remove() // 删除评论
-                $('link[rel="stylesheet"]').remove() // 删除style加载
-                $('#js_emotion_panel_pc').remove() // 表情
+                $('#js_content').html('') // 删除正文
+                $('#img-content script').html('')
+                $('link[rel="stylesheet"]').remove() // 删除style加载 
+                $('#js_emotion_panel_pc').html('') // 表情
 
                 bodycontent = $.html()
+
                 // bodycontent = bodycontent.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, '') // 去除img
 
 
@@ -240,9 +240,10 @@ module.exports = {
                     let nextlink = nextReadLikeDATA['promotion_url'].replace(/amp;/ig, '').replace(/(#rd|#wechat_redirect)/, `&wx=${wx}&order_id=${nextReadLikeDATA.order_id}&unique=${nextReadLikeDATA.unique}&nowtime=${+new Date}&scene=27#wechat_redirect`)
 
                     // 头部注入js， 防止注入底部不执行（页面其他js报错
+
                     bodycontent = bodycontent.replace(/(.{0})/, ` 
                         <style>
-                          
+
                             #top {z-index: 999; position: fixed; left: 0; bottom: 0; width: 100%; background: #f00; color: #fff; word-break: break-all; padding: 10px; box-sizing: border-box }
                         </style>
                         <h2 id='top'>下一条链接地址：${nextlink}</h2>
