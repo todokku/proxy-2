912cf77a2490c12f3e0c5fa401578b9188a46451
wulala <l>
2020-04-17 11:08:20 +0800

    添加超时

--- mongodb/serverdata.js
+++ mongodb/serverdata.js
@@ -59,7 +59,7 @@ serverAction.getFindAll = async (wx, follow, num = 4) => {
     if (hour >= 18 && hour <= 23) num = 3
 
 
-    let resDATA = await axios.get(`https://s.yundiao365.com/crawler/index/publics?&machine_num=${wx}&limit_num=${num}&follow=${follow}`, timeout: 10000).catch(async err => {
+    let resDATA = await axios.get(`https://s.yundiao365.com/crawler/index/publics?&machine_num=${wx}&limit_num=${num}&follow=${follow}`, { timeout: 10000 }).catch(async err => {
         return await serverAction.recordErrNet(err, 'getFindAll').catch(err => ({
             error: true
         }))
@@ -125,7 +125,7 @@ serverAction.getReadLikeAll = async (wx, num = 60) => {
     num = newaccount ? 20 : num + over2
     let url_type = exact ? 'exactArticle' : 'articleLinks'
 
-    let resDATA = await axios.get(`https://s.yundiao365.com/crawler/index/${url_type}?machine_num=${wx}&limit_num=${num}`).catch(async err => {
+    let resDATA = await axios.get(`https://s.yundiao365.com/crawler/index/${url_type}?machine_num=${wx}&limit_num=${num}`, { timeout: 10000 }).catch(async err => {
         return await serverAction.recordErrNet(err, 'getReadLikeAll').catch(err => ({
             error: true
         }))
